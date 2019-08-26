57920577d52e274eef75c51797a01ee37e22bcc5
Octave Rossi <ocrossi@z1r11p4.le-101.fr>
2019-08-26 18:12:48 +0200

    v1.0.4 +norm

--- src/main_src/output_ants.c
+++ src/main_src/output_ants.c
@@ -6,7 +6,7 @@
 /*   By: ocrossi <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/07/11 13:33:07 by ocrossi      #+#   ##    ##    #+#       */
-/*   Updated: 2019/08/08 21:29:16 by ocrossi     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/08/26 17:49:03 by ocrossi     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -30,10 +30,8 @@ void	get_ant_move_ovf(t_opt *opt, char **s1, char **s2, UINT len)
 	UINT ovf;
 
 	ovf = BUF_SIZE + opt->save;
-	//FPF("ovf = %u buf size = %u save = %u\n", ovf, BUF_SIZE, opt->save);
 	while (ovf <= len)
 	{
-//		FPF("hi\n");
 		*s1 = ft_strnew(BUF_SIZE);
 		*s2 = ft_strdup(opt->out[opt->lap_cp]);
 		ft_strdel(&(opt->out[opt->lap_cp]));
@@ -49,36 +47,19 @@ void	get_ant_move(t_opt *opt, UINT (*lenght)[], t_data *data)
 	char *r2;
 	char *r3;
 
-//	FPF("salut get ant move lap = %u\n", opt->lap_cp);
-	opt->save = (*lenght)[opt->lap_cp]; //  le pb doit se trouver ici
-//	FPF("opt->save %u\n", opt->save);
-//	FPF("room = %s\n", data->r_tab[opt->room_id].name);
-	//FPF("hey ta race toi %u\n", data->r_tab[opt->room_id].len_name);	
-//	FPF("av calcul la len rentree = %u\n", (*lenght)[opt->lap_cp]);
-//	ft_putendl(opt->out[opt->lap_cp]);
-//	print_tab(lenght, data->lap);	
+	opt->save = (*lenght)[opt->lap_cp];
 	(*lenght)[opt->lap_cp] = 3 + data->r_tab[opt->room_id].len_name +
 			count_digits(opt->ants) + (*lenght)[opt->lap_cp];
-	//FPF("la len actuellement rentree = %u\n", (*lenght)[opt->lap_cp]);
-	//FPF("salut get ant move lap = %u\n", opt->lap_cp);
-//	ft_putendl(opt->out[opt->lap_cp]);
 	if ((*lenght)[opt->lap_cp] >= BUF_SIZE)
-	{
-	//	FPF("jourbon\n");
 		get_ant_move_ovf(opt, &r1, &r2, (*lenght)[opt->lap_cp]);
-	}
 	r1 = ft_strjoin("L", ft_litoa((ULL)opt->ants));
 	r2 = ft_strjoin(r1, ft_strjoin("-",
 					ft_strjoin(data->r_tab[opt->room_id].name, " ")));
 	ft_strdel(&r1);
-//	if (ft_strlen(opt->out[opt->lap_cp]))
 	r3 = ft_strdup(opt->out[opt->lap_cp]);
 	ft_strdel(&(opt->out[opt->lap_cp]));
 	opt->out[opt->lap_cp] = ft_strjoin(r3, r2);
 	del_temp_str(&r2, &r3, NULL);
-//	ft_putendl(opt->out[opt->lap_cp]);
-//	FPF("end of get ant move ligne actu = %s\n", opt->out[opt->lap_cp]);
-//	ft_putstr_2d(opt->out);
 }
 
 void	ant_march(t_opt *opt, t_data *data, UINT i, UINT (*lenght)[])
@@ -87,17 +68,11 @@ void	ant_march(t_opt *opt, t_data *data, UINT i, UINT (*lenght)[])
 
 	j = 4;
 	opt->lap_cp = opt->lap;
-//	FPF("opt->lap = %u data->lap %u taille du chemin a inserer %u\n", opt->lap, data->lap, data->ret[i][0]);
-	//print_final_path_tab(data);
-//	FPF("allo ant march i = %u taille du path = %u\n", i, data->paths[i][1] - 4);
 	while (j < data->paths[i][1])
 	{
-//		FPF("j = %u\n", j);
 		opt->room_id = data->paths[i][j];
-//		FPF("->AM room = %s\n", data->r_tab[opt->room_id].name);
 		get_ant_move(opt, lenght, data);
 		opt->lap_cp++;
 		j++;
 	}
-//	FPF("-------------------------------------------\n");
 }
